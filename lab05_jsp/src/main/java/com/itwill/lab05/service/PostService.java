d54d5eda2e7f9e90dcd858446f61f994e9b81ce8
Youngwung <chenld0728@gmail.com>
2024-05-29 19:20:37 +0900

    read메서드 추가

--- lab05_jsp/src/main/java/com/itwill/lab05/service/PostService.java
+++ lab05_jsp/src/main/java/com/itwill/lab05/service/PostService.java
@@ -13,28 +13,40 @@ import com.itwill.lab05.repository.PostDao;
 // Controller(Web) 계층에게 비즈니스 로직 결과를 리턴.
 public enum PostService {
 	INSTANCE;
-	
+
 	private static final Logger log = LoggerFactory.getLogger(PostService.class);
-	
+
 	// Persistence(Repository) 계층의 기능(메서드)들을 사용하기 위해서.
 	private final PostDao postDao = PostDao.INSTANCE;
-	
+
 	public List<Post> read() { // Service는 Controller로 사용하기 위해 public로 사용
 		log.debug("read()");
 		List<Post> list = postDao.select();
 		log.debug("list size = {}", list.size());
 		return list;
 	}
-	
+
 	public int create(Post post) {
 		log.debug("create()");
 		log.debug("create(post = {})", post);
-		
+
 		// Repository 계층의 메서드를 사용해서 DB 테이블에 행을 삽입(insert)
 		// Dao에 접근해서 db에 데이터를 삽입
 		int result = postDao.insert(post);
 		log.debug("result = {}", result);
-		
+
 		return result; // insert된 행의 개수를 리턴.
 	}
+
+	public Post read(int id) {
+		log.debug("id = {}", id);
+
+		// 영속성 계층의 메서드를 호출해서 DB 테이블에서 id로 검색하는 SQL을 실행.
+		Post post = postDao.select(id);
+		log.debug("{}", post);
+
+		return post; // 컨트롤러에게 검색한 Post 객체를 리턴.
+		// *얘는 컨트롤러가 호출함. PostDetailsController.java
+	}
+
 }
